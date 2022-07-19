616d445008c033299ff7516f2e9aa90edecbf57c
Lucas Coronel <lccoronel7@icloud.com>
2022-07-19 07:52:47 -0300

    ✨ student repositories

---
+++ modules/student/repositories/studentRepository.go
@@ -0,0 +1,32 @@
+package repositories
+
+import (
+	"main/database"
+	"main/helpers"
+	"main/modules/student/entities"
+)
+
+func FindStudentById(id string) entities.Student {
+	var existsStudent entities.Student
+
+	for i := 0; i < len(database.StudentsList); i++ {
+		if database.StudentsList[i].Id.String() == id {
+			existsStudent = database.StudentsList[i]
+		}
+	}
+
+	return existsStudent
+}
+
+func DeleteStudentById(id string) bool {
+	var deletedUser bool = false
+
+	for i := 0; i < len(database.StudentsList); i++ {
+		if database.StudentsList[i].Id.String() == id {
+			helpers.RemoveIndex(database.StudentsList, i)
+			deletedUser = true
+		}
+	}
+
+	return deletedUser
+}
